16520d80b79dd468f76b7589177ef886971824fe
IO31-WEB <ISAIAHOLIVER95@YAHOO.COM>
2026-03-10 16:49:47 -0400

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,12 +21,7 @@ const isPublicRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-    const { userId } = await auth()
-    if (!userId) {
-      const signInUrl = new URL('/sign-in', request.url)
-      signInUrl.searchParams.set('redirect_url', request.url)
-      return NextResponse.redirect(signInUrl)
-    }
+    await auth.protect()
   }
 })
 
